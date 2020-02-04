72d72a898a9362d7a039d3739057987b4c8071f9
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-04 03:17:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,3 +68,76 @@ end
 
 
 
+# holiday_hash = {
+#   :winter => {
+#       :christmas => ["Lights", "Wreath"],
+#       :new_years => ["Party Hats"]
+#   },
+#   :summer => {
+#       :fourth_of_july => ["Fireworks", "BBQ"]
+#   },
+#   :fall => {
+#       :thanksgiving => ["Turkey"]
+#   },
+#   :spring => {
+#       :memorial_day => ["BBQ"]
+#   }
+# }
+
+# # Write a method that returns the second supply for the Fourth of July.
+# def second_supply_for_fourth_of_july(holiday_hash)
+#   holiday_hash[:summer][:fourth_of_july][1]
+# end
+
+# # Write a method that adds a supply to both Winter holidays.
+# def add_supply_to_winter_holidays(holiday_hash, supply)
+#   holiday_hash[:winter].each { |key, value| holiday_hash[:winter][key] << supply}
+# end
+
+# # Write a method that adds a supply to Memorial Day.
+# def add_supply_to_memorial_day(holiday_hash, supply)
+#   holiday_hash[:spring][:memorial_day] << supply
+# end
+
+# # Write a method that adds a new holiday and its associated supplies to any season.
+# def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+#   holiday_hash[season] = {holiday_name => supply_array}
+# end
+
+# # Write a method to collect all Winter supplies from all the winter holidays.
+# def all_winter_holiday_supplies(holiday_hash)
+#   holiday_hash[:winter].values.flatten
+# end
+
+# # Write a method that uses a loop to list out all the supplies you have for each holiday and the season.
+# def all_supplies_in_holidays(holiday_hash)
+#   holiday_hash.each do |season, holiday|
+#       #1st capitalize the season names
+#       puts "#{season.to_s.capitalize!}:"
+#       #2nd go down to holiday's names hash
+#       holiday.each do |name, supply|
+#         new_array = []
+#         array = name.to_s.split("_#split all the words with _
+#         #capitalize the names
+#         array.each { |word| new_array << word.capitalize! }
+#         #join separate words with " "
+#         name = new_array.join(" ")
+#         #turn array to string with join 
+#         supply = supply.join(", ")
+#         puts "  #{name}: #{supply}"
+#       end
+#     end
+# end
+
+# # Write a method to collect all holidays with "BBQ" in the supply array.
+# def all_holidays_with_bbq(holiday_hash)
+#   array = []
+#   holiday_hash.each do |season, holiday|
+#       holiday.each do |name, supply|
+#       if supply.include?("BBQ")
+#         array << name
+#       end
+#       end
+#   end  
+#   array
+# end
